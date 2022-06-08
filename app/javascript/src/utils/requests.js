9a4c72c00ca6bf17a1228d869cf1ab5dedd6cf66
jones89-dan <jones.89.dan@gmail.com>
2022-06-07 20:14:45 -0700

    Created the createProperty ajax request

--- app/javascript/src/utils/requests.js
+++ app/javascript/src/utils/requests.js
@@ -8,3 +8,32 @@ $.ajaxSetup({
     console.log(request, errorMessage);
   }
 });
+
+export var createProperty = function (title, description, city, country, property_type, price_per_night, max_guests, bedrooms, beds, baths, image, callback) {
+  var fromData = new FormData();
+  if (image) {
+    formData.append('property[image]', image);
+  }
+  
+  var request = {
+    type: 'POST',
+    url: 'api/properties',
+    data: {
+      title: title,
+      description: description,
+      city: city,
+      country: country,
+      property_type: property_type,
+      price_per_night: price_per_night,
+      max_guests: max_guests,
+      bedrooms: bedrooms,
+      beds: beds,
+      baths: baths,
+      formData,
+    },
+    success: function (response) {
+      callback(response);
+    }
+  };
+  $.ajax(request);
+};
