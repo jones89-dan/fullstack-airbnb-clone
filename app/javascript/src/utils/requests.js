d8aeade54161ba2a4546173f24a9b1479f858bb0
jones89-dan <jones.89.dan@gmail.com>
2022-06-07 19:58:13 -0700

    Added jquery and set up ajax request header

---
+++ app/javascript/src/utils/requests.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+  },
+  error: function (request, errorMessage) {
+    console.log(request, errorMessage);
+  }
+});
