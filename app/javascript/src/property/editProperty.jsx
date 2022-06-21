d301e6f300134e83832f10f069d2adb200d66478
jones89-dan <jones.89.dan@gmail.com>
2022-06-21 13:05:28 -0700

    Testing fetch request for edit

--- app/javascript/src/property/editProperty.jsx
+++ app/javascript/src/property/editProperty.jsx
@@ -13,6 +13,40 @@ class EditProperty extends React.Component {
             [e.target.name]: e.target.value,
         })
     }
+
+    editBooking = (e) => {
+        if (e) { e.preventDefault(); }
+        fetch(`/api/properties/${this.props.property_id}`, safeCredentials({
+            method: 'PUT',
+            body: JSON.stringify({
+                property: {
+                    title: this.state.title,
+                    description: this.state.description,
+                    city: this.state.city,
+                    country: this.state.country,
+                    property_type: this.state.property_type,
+                    price_per_night: this.state.price_per_night,
+                    max_guests: this.state.max_guests,
+                    bedrooms: this.state.bedrooms,
+                    beds: this.state.beds,
+                    baths: this.state.baths,
+                    image: this.state.image,
+                }
+            })
+        }))
+            .then(handleErrors)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    render () {
+
+    }
+
 }
 
 export default EditProperty;
