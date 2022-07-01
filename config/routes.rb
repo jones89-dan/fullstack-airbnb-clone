fc13345e093c08ed5e7e78058601d53d2ec1cc9c
jones89-dan <jones.89.dan@gmail.com>
2022-07-01 12:33:42 -0700

    Added bookingIndex route

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,9 @@ Rails.application.routes.draw do
     get '/authenticated'           => 'sessions#authenticated'
     delete '/sessions'             => 'sessions#destroy'
 
+    # booking
+    get '/BookingIndex'            => 'bookings#index'
+
     # stripe webhook
     post '/charges/mark_complete'  => 'charges#mark_complete'
 
