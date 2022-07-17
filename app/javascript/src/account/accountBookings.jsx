d848ed353ea54a704bbde1a45262d10e6b53e35d
jones89-dan <jones.89.dan@gmail.com>
2022-07-17 13:46:58 -0700

    Added Layout to accountBookings

--- app/javascript/src/account/accountBookings.jsx
+++ app/javascript/src/account/accountBookings.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM, { useParams } from 'react-dom';
+import Layout from '@src/layout';
 import 'react-dates/initialize';
 import { safeCredentials, handleErrors } from '@utils/fetchHelper';
 import $ from 'jquery';
@@ -30,24 +31,26 @@ class UserBookings extends React.Component {
     const { bookings, booking } = this.state;
 
     return (
-      <div className="container pt-4">
-        <h4 className="mb-1">Your Bookings!</h4>
-        <p className="text-secondary mb-3"></p>
-        <div className="text-center">
+      <Layout>
+        <div className="container pt-4">
+          <h4 className="mb-1">Your Bookings!</h4>
+          <p className="text-secondary mb-3"></p>
+          <div className="text-center">
 
-        </div>
-        <div className="row">
-          {bookings.map(booking => {
-            return (
-              <ul key={booking.id}>
-                <li>{booking.property.title}</li>
-                <li>{booking.start_date}</li>
-                <li>{booking.end_date}</li>
-              </ul>
-            )
-          })}
+          </div>
+          <div className="row">
+            {bookings.map(booking => {
+              return (
+                <ul key={booking.id}>
+                  <li>{booking.property.title}</li>
+                  <li>{booking.start_date}</li>
+                  <li>{booking.end_date}</li>
+                </ul>
+              )
+            })}
+           </div>
          </div>
-       </div>
+       </Layout>
     )
   }
 }
