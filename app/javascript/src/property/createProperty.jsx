3e755018ab8cf5bce08586e9ea0b8671b5baa734
jones89-dan <jones.89.dan@gmail.com>
2022-08-15 20:25:35 -0700

    Added containers to edit and create properites

--- app/javascript/src/property/createProperty.jsx
+++ app/javascript/src/property/createProperty.jsx
@@ -68,47 +68,49 @@ const CreateProperty = () => {
 
   return (
     <Layout>
-      <form className="p-2 rounded" onSubmit={newProperty}>
-        <div className="new-to-t">
-          <h1>Add a Property</h1>
-        </div>
-        <div className="form-group">
-          <input type="text" className="form-control title" placeholder="Title"/>
-        </div>
-        <div className="form-group">
-          <input type="text" className="form-control description" placeholder="Description"/>
-        </div>
-        <div className="form-group">
-          <input type="text" className="form-control city" placeholder="City"/>
-        </div>
-        <div className="form-group">
-          <input type="text" className="form-control country" placeholder="Country"/>
-        </div>
-        <div className="form-group">
-          <input type="text"className="form-control property-type" placeholder="Property Type"/>
-        </div>
-        <div className="form-group">
-          <input type="text" className="form-control price-per-night" placeholder="Price Per Night"/>
-        </div>
-        <div className="form-group">
-          <input type="text" className="form-control max-guests" placeholder="Max Guests"/>
-        </div>
-        <div className="form-group">
-          <input type="text" className="form-control bedrooms" placeholder="Bedrooms"/>
-        </div>
-        <div className="form-group">
-          <input type="text" className="form-control beds" placeholder="Beds"/>
-        </div>
-        <div className="form-group">
-          <input type="text" className="form-control baths" placeholder="Baths"/>
-        </div>
+     <div className="container pt-4">
+        <form className="p-2 rounded" onSubmit={newProperty}>
+          <div className="new-to-t">
+            <h1>Add a Property</h1>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control title" placeholder="Title"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control description" placeholder="Description"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control city" placeholder="City"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control country" placeholder="Country"/>
+          </div>
+          <div className="form-group">
+            <input type="text"className="form-control property-type" placeholder="Property Type"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control price-per-night" placeholder="Price Per Night"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control max-guests" placeholder="Max Guests"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control bedrooms" placeholder="Bedrooms"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control beds" placeholder="Beds"/>
+          </div>
+          <div className="form-group">
+            <input type="text" className="form-control baths" placeholder="Baths"/>
+          </div>
 
-        <label id="upload-image-btn" htmlFor="imageUpload">Upload image</label>
-        <img id="image-preview" src="" style={{display: 'none'}} alt="image preview"/>
-        <input type="file" id="imageUpload" name="image" accept="image/*" onChange={handleImage}></input>
+          <label id="upload-image-btn" htmlFor="imageUpload">Upload image</label>
+          <img id="image-preview" src="" style={{display: 'none'}} alt="image preview"/>
+          <input type="file" id="imageUpload" name="image" accept="image/*" onChange={handleImage}></input>
 
-        <button type="submit" id="sign-up-btn" className="btn btn-default btn-warning pull-right">Add Property</button>
-      </form>
+          <button type="submit" id="sign-up-btn" className="btn btn-default btn-warning pull-right">Add Property</button>
+        </form>
+      </div
     </Layout>
 
   )
--- app/javascript/src/property/editProperty.jsx
+++ app/javascript/src/property/editProperty.jsx
@@ -79,21 +79,23 @@ class EditProperty extends React.Component {
 
       return (
         <Layout>
-          <h3>Make changes to your property</h3>
-            <form onSubmit={this.editExistingProperty}>
-              <input name='title' className="form-control form-control-lg mb-3" type='text' placeholder={title} value={this.state.title || ""} onChange={this.handleChange}  />
-              <input name='description' className="form-control form-control-lg mb-3" type='text' placeholder={description} value={this.state.description || ""} onChange={this.handleChange}  />
-              <input name='city' className="form-control form-control-lg mb-3" type='text' placeholder={city} value={this.state.city || ""} onChange={this.handleChange}  />
-              <input name='country' className="form-control form-control-lg mb-3" type='text' placeholder={country} value={this.state.country || ""} onChange={this.handleChange}  />
-              <input name='property_type' className="form-control form-control-lg mb-3" type='text' placeholder={property_type} value={this.state.property_type || ""} onChange={this.handleChange}  />
-              <input name='price_per_night' className="form-control form-control-lg mb-3" type='number' placeholder={price_per_night} value={this.state.price_per_night || ""} onChange={this.handleChange}  />
-              <input name='max_guests' className="form-control form-control-lg mb-3" type='number' placeholder={max_guests} value={this.state.max_guests || ""} onChange={this.handleChange}  />
-              <input name='bedrooms' className="form-control form-control-lg mb-3" type='number' placeholder={bedrooms} value={this.state.bedrooms || ""} onChange={this.handleChange}  />
-              <input name='beds' className="form-control form-control-lg mb-3" type='number' placeholder={beds} value={this.state.beds || ""} onChange={this.handleChange}  />
-              <input name='baths' className="form-control form-control-lg mb-3" type='number' placeholder={baths} value={this.state.baths || ""} onChange={this.handleChange}  />
+         <div className="container pt-4">
+            <h3>Make changes to your property</h3>
+              <form onSubmit={this.editExistingProperty}>
+                <input name='title' className="form-control form-control-lg mb-3" type='text' placeholder={title} value={this.state.title || ""} onChange={this.handleChange}  />
+                <input name='description' className="form-control form-control-lg mb-3" type='text' placeholder={description} value={this.state.description || ""} onChange={this.handleChange}  />
+                <input name='city' className="form-control form-control-lg mb-3" type='text' placeholder={city} value={this.state.city || ""} onChange={this.handleChange}  />
+                <input name='country' className="form-control form-control-lg mb-3" type='text' placeholder={country} value={this.state.country || ""} onChange={this.handleChange}  />
+                <input name='property_type' className="form-control form-control-lg mb-3" type='text' placeholder={property_type} value={this.state.property_type || ""} onChange={this.handleChange}  />
+                <input name='price_per_night' className="form-control form-control-lg mb-3" type='number' placeholder={price_per_night} value={this.state.price_per_night || ""} onChange={this.handleChange}  />
+                <input name='max_guests' className="form-control form-control-lg mb-3" type='number' placeholder={max_guests} value={this.state.max_guests || ""} onChange={this.handleChange}  />
+                <input name='bedrooms' className="form-control form-control-lg mb-3" type='number' placeholder={bedrooms} value={this.state.bedrooms || ""} onChange={this.handleChange}  />
+                <input name='beds' className="form-control form-control-lg mb-3" type='number' placeholder={beds} value={this.state.beds || ""} onChange={this.handleChange}  />
+                <input name='baths' className="form-control form-control-lg mb-3" type='number' placeholder={baths} value={this.state.baths || ""} onChange={this.handleChange}  />
 
-              <button type="submit" className="btn btn-danger btn-block btn-lg">Update!</button>
-            </form>
+                <button type="submit" className="btn btn-danger btn-block btn-lg">Update!</button>
+              </form>
+            </div>
           </Layout>
       )
     }
